83a4d6f3117dea2e04743657fcc8060b99875908
KoJongHyeok <108115196+KoJongHyeok@users.noreply.github.com>
2022-10-25 17:31:39 +0900

    Update MVC11_YoilTellerMVC1.java

--- MVC11_YoilTellerMVC1.java
+++ MVC11_YoilTellerMVC1.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class YoilTellerMVC1 {
 	
-	@RequestMapping("/getYoilMVC1")
+	@RequestMapping("/getYoilMVC")
 	public String main(int year, int month, int day, Model model) throws IOException {
 		
 //		1. 유효성 검사
