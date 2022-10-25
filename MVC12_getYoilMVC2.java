8103e451fb0a35e023b474cb585aba88f42e4902
KoJongHyeok <108115196+KoJongHyeok@users.noreply.github.com>
2022-10-25 15:25:25 +0900

    Create MVC12_getYoilMVC2.java

---
+++ MVC12_getYoilMVC2.java
@@ -0,0 +1,49 @@
+package com.fastcampus.ch2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class YoilTellerMVC {
+	
+	@RequestMapping("/getYoilMVC2")
+	public void main(int year, int month, int day, Model model) throws IOException {
+		
+//		1. 유효성 검사
+//		if (!isValid(year, month, day)) { return "yoilError"; }
+		
+//		2. 요일 계산
+		char yoil = getYoil(year, month, day);
+		
+//		3. 계산 결과를 model에 저장
+		model.addAttribute("year", year);
+		model.addAttribute("month", month);
+		model.addAttribute("day", day);
+		model.addAttribute("yoil", yoil);
+		
+//		return "yoil";	// /WEB-INF/views/yoil.jsp
+		
+	}
+
+	
+	
+	private boolean isValid(int year, int month, int day) {
+		return true;
+	}
+
+
+
+	private char getYoil(int year, int month, int day) {
+		Calendar cal = Calendar.getInstance();
+		cal.set(year, month-1, day);
+		
+		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+		return " 일월화수목금토".charAt(dayOfWeek);
+	}
+
+}
