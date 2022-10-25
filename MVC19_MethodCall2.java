6b9faaf1a78c00eaee232ec72a19ae6c7408b4bd
KoJongHyeok <108115196+KoJongHyeok@users.noreply.github.com>
2022-10-25 17:36:10 +0900

    Update MVC19_MethodCall2.java

--- MVC19_MethodCall2.java
+++ MVC19_MethodCall2.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 import org.springframework.ui.Model;
 import org.springframework.validation.support.BindingAwareModelMap;
 
-public class MethodCall2 {
+public class MethodCall {
 	public static void main(String[] args) throws Exception{
 
 		Class clazz = Class.forName("com.fastcampus.ch2.YoilTellerMVC");
