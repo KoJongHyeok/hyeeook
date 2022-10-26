6c45f9ed99811db3c8c808271aafdd77193dfeaa
KoJongHyeok <108115196+KoJongHyeok@users.noreply.github.com>
2022-10-26 14:49:49 +0900

    Create MVC28_PerformanceFilter.java

---
+++ MVC28_PerformanceFilter.java
@@ -0,0 +1,41 @@
+package com.fastcampus.ch2;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+// 필터를 적용할 요청의 패턴 지정 - 모든 요청에 필터를 적용.
+@WebFilter(urlPatterns="/*")
+public class PerformanceFilter implements Filter {  // 수행 시간을 측정하는 필터
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// 초기화 작업
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// 1. 전처리 작업(시작시간 저장)
+		long startTime = System.currentTimeMillis();
+
+		// 2. 서블릿 또는 다음 필터를 호출
+		chain.doFilter(request, response); 
+		
+		// 3. 후처리 작업(현재시간 - 시작시간 = 소요시간)
+		System.out.print("["+((HttpServletRequest)request).getRequestURI()+"]");
+		System.out.println(" 소요시간="+(System.currentTimeMillis()-startTime)+"ms");
+	}
+
+	@Override
+	public void destroy() {
+		// 정리 작업
+	}
+
+}
