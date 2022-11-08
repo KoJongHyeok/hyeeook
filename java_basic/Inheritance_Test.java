d6649f1573487e57753d0a166f2f9b07706f7e7c
KoJongHyeok <108115196+KoJongHyeok@users.noreply.github.com>
2022-11-09 00:30:02 +0900

    Create Inheritance_Test.java

---
+++ java_basic/Inheritance_Test.java
@@ -0,0 +1,29 @@
+interface Inheritance_Interface {
+	public abstract void inheritanceTest();
+}
+
+class Inheritance_Parent implements Inheritance_Interface {
+	public void inheritanceTest() {
+		System.out.println("inheritanceTest()");
+	}
+}
+
+class Inheritance_Child extends Inheritance_Parent {}
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		Inheritance_Interface inheritTest_Parent = new Inheritance_Parent();
+		Inheritance_Interface inheritTest_Child = new Inheritance_Child();
+		inheritTest_Parent.inheritanceTest();
+		inheritTest_Child.inheritanceTest();
+		
+	}
+
+}
+
+/*
+inheritanceTest()
+inheritanceTest()
+*/
