d3330c933705368e757ef0075d8ae0c49be15bc3
KoJongHyeok <108115196+KoJongHyeok@users.noreply.github.com>
2022-11-10 15:04:17 +0900

    Create AOP_MyMath.java

---
+++ spring_basic/ch3/AOP_MyMath.java
@@ -0,0 +1,22 @@
+package com.fastcampus.ch3.aop;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyMath {       // 핵심 기능(target)으로 사용할 클래스
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+    public int add(int a, int b, int c) {
+        return a+b+c;
+    }
+
+    public int subtract(int a, int b) {
+        return a-b;
+    }
+
+    public int multiply(int a, int b) {
+        return a*b;
+    }
+}
