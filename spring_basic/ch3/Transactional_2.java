800ed84d5bced751cd0d747a8eb787fc0446b35b
KoJongHyeok <108115196+KoJongHyeok@users.noreply.github.com>
2022-11-11 16:01:02 +0900

    Create Transactional_2.java

---
+++ spring_basic/ch3/Transactional_2.java
@@ -0,0 +1,56 @@
+package com.fastcampus.ch3;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+// ************************** TransactionManager를 자동으로 주입해서 처리하는 코드 **************************
+
+@Repository
+public class A1Dao {
+
+    @Autowired
+    DataSource ds;
+
+    public int insert(int key, int value) throws Exception {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            conn = DataSourceUtils.getConnection(ds);
+            pstmt = conn.prepareStatement("insert into a1 values(?, ?)");
+            pstmt.setInt(1, key);
+            pstmt.setInt(2, value);
+            System.out.println("conn = " + conn);
+
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            close(pstmt);
+            DataSourceUtils.releaseConnection(conn, ds);
+        }
+    }
+
+    private void close(AutoCloseable... acs) {
+        for (AutoCloseable ac : acs) {
+            try{ac.close();}
+            catch(Exception e) {e.printStackTrace();}
+        }
+    }
+
+    public void deleteAll() throws Exception {
+        Connection conn = ds.getConnection();
+        String sql = "delete from a1";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.executeUpdate();
+        close(pstmt);
+    }
+
+}
