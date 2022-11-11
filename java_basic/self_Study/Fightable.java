ac7618db3f70585992b934cee146dd3bd7df0036
KoJongHyeok <108115196+KoJongHyeok@users.noreply.github.com>
2022-11-11 10:15:17 +0900

    Create Fightable.java

---
+++ java_basic/self_Study/Fightable.java
@@ -0,0 +1,12 @@
+
+public interface Fightable {
+	
+//  ----------------------------------------------------------------------
+//  >> 변수
+
+//  ----------------------------------------------------------------------
+//  >> 메서드
+	public abstract void move(int x, int y);
+	public abstract void attack(Fightable f);
+
+}
