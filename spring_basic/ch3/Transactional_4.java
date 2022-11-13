fbeadee327345fecd2218fe7510f208bc0d5162b
KoJongHyeok <108115196+KoJongHyeok@users.noreply.github.com>
2022-11-13 14:54:44 +0900

    Update Transactional_4.java

--- spring_basic/ch3/Transactional_4.java
+++ spring_basic/ch3/Transactional_4.java
@@ -17,3 +17,59 @@
 - NEVER : Tx없이 처리. Tx이 진행 중이면 예외 발생.
 */
 
+//-----------------------------------------------------------------------------------------------------------
+
+package com.fastcampus.ch3;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@Repository
+public class B1Dao {
+
+    @Autowired
+    DataSource ds;
+
+    public int insert(int key, int value) throws Exception {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            conn = DataSourceUtils.getConnection(ds);
+            pstmt = conn.prepareStatement("insert into b1 values(?, ?)");
+            pstmt.setInt(1, key);
+            pstmt.setInt(2, value);
+            System.out.println("conn = " + conn);
+
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            close(pstmt);
+            DataSourceUtils.releaseConnection(conn, ds);
+        }
+    }
+
+    private void close(AutoCloseable... acs) {
+        for (AutoCloseable ac : acs) {
+            try{ac.close();}
+            catch(Exception e) {e.printStackTrace();}
+        }
+    }
+
+    public void deleteAll() throws Exception {
+        Connection conn = ds.getConnection();
+        String sql = "delete from a1";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.executeUpdate();
+        close(pstmt);
+    }
+
+}
