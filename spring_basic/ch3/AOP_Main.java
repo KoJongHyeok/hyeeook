f2225ba8da726467d48b0331bea4acf91b550cda
KoJongHyeok <108115196+KoJongHyeok@users.noreply.github.com>
2022-11-22 12:27:19 +0900

    Update and rename AOP_AopMain2.java to AOP_Main.java

--- spring_basic/ch3/AOP_Main.java
+++ spring_basic/ch3/AOP_Main.java
@@ -3,7 +3,7 @@ package com.fastcampus.ch3.aop;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
-public class AopMain2 {
+public class Main {
     public static void main(String[] args) {
 
         ApplicationContext ac = new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/**/root-context_aop.xml");
