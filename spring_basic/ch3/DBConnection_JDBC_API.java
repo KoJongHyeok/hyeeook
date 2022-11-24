c3ec684c6eaf77adb1630d4b5f88590f7616d98f
KoJongHyeok <108115196+KoJongHyeok@users.noreply.github.com>
2022-11-24 10:47:25 +0900

    Create DBConnection_JDBC_API.java

---
+++ spring_basic/ch3/DBConnection_JDBC_API.java
@@ -0,0 +1,31 @@
+package com.fastcampus.ch3;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DBConnectionTest {
+	
+	public static void main(String[] args) throws Exception {
+		// *** JDBC API를 이용하여 DB에 접근하기.
+		
+		String DB_URL =					//-------------------------------- DB에 접속하기 위한 URL
+				"jdbc:mysql://localhost:3306/springbasic?useUnicode=true&characterEncoding=utf-8";
+		String DB_USER = "root";		//-------------------------------- DB ID
+		String DB_PASSWORD = "mysql";	//-------------------------------- DB PWD
+		
+		Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);	// DB에 대한 연결 얻기.
+		Statement stmt = conn.createStatement();	// 연결된 DB에 SQL 명령문을 사용하기 위한 Statement 생성.
+		
+		String query = "SELECT now()";				// SQL 명령문
+		ResultSet rs = stmt.executeQuery(query);	// SQL 명령문을 실행한 결과를 ResultSet에 저장.
+		
+		while(rs.next()) {
+			String curDate = rs.getString(1);
+			System.out.println(curDate);
+		}
+		
+	}
+	
+}
