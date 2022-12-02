e8ab5c5eb0060cfef801569c87db60463d1c0000
KoJongHyeok <108115196+KoJongHyeok@users.noreply.github.com>
2022-12-02 16:48:04 +0900

    Create Sort.java

---
+++ java_basic/Sort.java
@@ -0,0 +1,14 @@
+		
+
+int[] iArr = { 222, 9, 6, 77, 2, 54 };
+		
+		for (int i = 0; i < iArr.length; i++) {
+			for (int j = 0, tmp=0; j < iArr.length - 1; j++) {
+				if (iArr[j] > iArr[j + 1]) {
+					tmp = iArr[j];
+					iArr[j] = iArr[j + 1];
+					iArr[j + 1] = tmp;
+				}
+			}
+			System.out.println(Arrays.toString(iArr));
+		}
