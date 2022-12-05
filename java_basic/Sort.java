13439791ad8ab93fd1fa08a0f5c7faa9e6ae9627
KoJongHyeok <108115196+KoJongHyeok@users.noreply.github.com>
2022-12-05 18:25:06 +0900

    Update Sort.java

--- java_basic/Sort.java
+++ java_basic/Sort.java
@@ -1,35 +1,58 @@
-		
+public class Sort {
 
-int[] iArr = { 222, 9, 6, 77, 2, 54 };
+	public static void main(String[] args) throws Exception {
 
-// 막(?) 정렬
-for (int i = 0; i < iArr.length; i++) {
-	for (int j = 0, tmp=0; j < iArr.length - 1; j++) {
-		if (iArr[j] > iArr[j + 1]) {
-			tmp = iArr[j];
-			iArr[j] = iArr[j + 1];
-			iArr[j + 1] = tmp;
+		int[] iArr = { 9, 3, 6, 1, 4, 55, 2, 30 };
+		bubbleSort(iArr);
+		selectionSort(iArr);
+		insertionSort(iArr);
+		System.out.println(Arrays.toString(iArr));
+
+	}
+
+	static void swap(int[] arr, int x, int y) {
+		int tmp = 0;
+
+		tmp = arr[x];
+		arr[x] = arr[y];
+		arr[y] = tmp;
+	}
+	
+	static void bubbleSort(int[] iArr) {
+		for (int i = iArr.length - 1; i > 0; i--) {
+			for (int j = 0; j < i; j++) {
+				if (iArr[j] > iArr[j + 1]) {
+					swap(iArr, j, j + 1);
+				}
+			}
 		}
 	}
-	System.out.println(Arrays.toString(iArr));
-}
-
-// 버블 정렬
-for (int i=iArr.length-1; i>0; i--) {
-	for (int j=0, tmp=0; j<i; j++) {
-		if (iArr[j] > iArr[j + 1]) {
-			tmp = iArr[j];
-			iArr[j] = iArr[j + 1];
-			iArr[j + 1] = tmp;
+	
+	static void selectionSort(int[] iArr) {
+		int minIndex = 0;
+		for (int i = 0; i < iArr.length - 1; i++) {
+			minIndex = i;
+			
+			for (int j = i + 1; j < iArr.length; j++) {
+				if (iArr[minIndex] > iArr[j]) {
+					minIndex = j;
+				}
+			}
+			swap(iArr, minIndex, i);
 		}
 	}
-	System.out.println(Arrays.toString(iArr));
-}
+	
+	static void insertionSort(int[] iArr) {
+		int target = 0;
+		for (int i = 1; i < iArr.length; i++) {
+			int j = i - 1;
+			target = iArr[i];
+			while (j >= 0 && iArr[j] > target) {
+				iArr[j + 1] = iArr[j];
+				j--;
+			}
+			iArr[j + 1] = target;
+		}
+	}
 
-
-static void swap(int[] arr, int x, int y) {
-	int tmp = 0;
-	tmp = arr[x];
-	arr[x] = arr[y];
-	arr[y] = tmp;
 }
