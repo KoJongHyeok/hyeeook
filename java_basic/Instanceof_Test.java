13c3ea7830a5d7e81508a15d147071c779bcbcb9
KoJongHyeok <108115196+KoJongHyeok@users.noreply.github.com>
2022-12-07 14:52:11 +0900

    Update Instanceof_Test.java

--- java_basic/Instanceof_Test.java
+++ java_basic/Instanceof_Test.java
@@ -8,7 +8,10 @@ public class Instanceof_Test {
 		Car car = new Car();
 		SportsCar sportsCar = new SportsCar();
 		
+		// 생성된 실제 child 객체를 parent 타입의 참조변수로 사용해도 되냐는 의미.
 		boolean useChild_WithParent = sportsCar instanceof Car;	// true
+		
+		// 생성된 실제 parent 객체를 child 타입의 참조변수로 사용해도 되냐는 의미.
 		boolean useParent_WithChild = car instanceof SportsCar;	// false
 		
 		System.out.println(useChild_WithParent);
