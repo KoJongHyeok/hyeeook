9c3030d3362c37bc2f2b01059a9f2199a4ad78bc
KoJongHyeok <108115196+KoJongHyeok@users.noreply.github.com>
2022-12-08 11:12:31 +0900

    Update Comparable_Comparator.java

--- java_basic/Comparable_Comparator.java
+++ java_basic/Comparable_Comparator.java
@@ -11,3 +11,45 @@ public interface Comparator {	// 컴퓨터에 정렬 기준을 제시해줄 수 
 	// 반환값이 0 : 같음.
 	// 반환값이 - : 우측(o2)이 큼.
 }
+
+// -----------------------------------------------------------------------------------
+
+public class Comparable_Comparator_Test {
+
+	public static void main(String[] args) {
+		
+		String[] strArr = {"cat", "Dog", "lion", "tiger"};
+		
+		Arrays.sort(strArr);
+		System.out.println(Arrays.toString(strArr));
+		// [Dog, cat, lion, tiger]
+		
+		Arrays.sort(strArr, String.CASE_INSENSITIVE_ORDER);
+		System.out.println(Arrays.toString(strArr));
+		// [cat, Dog, lion, tiger]
+		
+		Arrays.sort(strArr, new Descending());
+		System.out.println(Arrays.toString(strArr));
+		// [tiger, lion, cat, Dog]
+		
+	}
+
+}
+
+	// static void sort(Object[] a, Comparator c)
+	//		>> sort() 메서드에 매개변수로 들어갈 수 있는 비교 기준은 'Comparator'만 가능.
+	// final class String implements ..., Comparable, ...
+	//		>> String 클래스는 Comparable 인터페이스를 구현.
+class Descending implements Comparator {
+	// sort() 메서드에 비교 기준으로 들어가는 Descending 클래스는 Comparator 인터페이스를 구현.
+	// 문자열(String)의 실제 비교 기준이 작성된 compare() 메서드 안에는 Comparable 인터페이스로 형변환.
+	@Override
+	public int compare(Object o1, Object o2) {
+		if (o1 instanceof Comparable && o2 instanceof Comparable) {
+			Comparable c1 = (Comparable) o1;
+			Comparable c2 = (Comparable) o2;
+			return c2.compareTo(c1);
+		}
+		return -1;
+	}
+}
