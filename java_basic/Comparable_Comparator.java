6b52811b3790e84c552e6fe378ef3ffc5e62c611
KoJongHyeok <108115196+KoJongHyeok@users.noreply.github.com>
2022-12-09 13:15:22 +0900

    Update Comparable_Comparator.java

--- java_basic/Comparable_Comparator.java
+++ java_basic/Comparable_Comparator.java
@@ -1,11 +1,11 @@
-public interface Comparable {	// 객체의 기본 정렬 기준.
+public interface Comparable {	// 객체의 기본 정렬 기준을 구현하는 데 사용.
 	int compareTo(Object o);	// 주어진 객체(o)를 자신(this)과 비교.
 	// 반환값이 1 : 본 객체(this)가 큼.
 	// 반환값이 0 : 같음.
 	// 반환값이 -1 : 매개변수 객체(o)가 큼.
 }
 
-public interface Comparator {	// 컴퓨터에 정렬 기준을 제시해줄 수 있는 인터페이스.
+public interface Comparator {	// 기본 정렬 기준 외에 다른 기준으로 정렬하고자 할 때 사용.
 	int compare(Object o1, Object o2);	// o1, o2 두 객체를 비교.
 	// 반환값이 + : 좌측(o1)이 큼.
 	// 반환값이 0 : 같음.
